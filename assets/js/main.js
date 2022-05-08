19ec57048bb62292516f143fd8b3a48310b0569a
yutingh <yutingh@cw.com.tw>
2022-05-08 15:07:51 +0800

    單一書本介紹頁

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+var width = $(window).width();
+
 $('.hamburger').click(function() {
   $('.hamburger, header, nav, .black').toggleClass('opened');
 });
@@ -18,4 +20,35 @@ $('.open-block').click(function() {
   $(this).siblings('.block').slideToggle();
   $(this).parent().siblings('li').children('.open-block').removeClass('opened');
   $(this).parent().siblings('li').children('.block').slideUp();
+});
+
+$('.card-video').click(function() {
+  let videoUrl = $(this).attr('data-video');
+  $('.modal iframe.embed-responsive-item').attr('src', `${videoUrl}?rel=0&autoplay=1`);
+  $('.modal').fadeIn();
+  $('.black').addClass('opened modal-black');
+});
+$('.modal-close').click(function() {
+  $('.modal iframe.embed-responsive-item').attr('src', '');
+  $('.modal').fadeOut();
+  $('.black').removeClass('opened modal-black');
+});
+
+$('a.smooth-scroll[href*="#"]:not([href="#"])').click(function () {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      let headerOffset = 71;
+      if ( width >=  1024 ) {
+        headerOffset = 170;
+      } else {
+        headerOffset = 71;
+      }
+      $('html, body').animate({
+        scrollTop: (target.offset().top - headerOffset)
+      }, 1000);
+      return false;
+    }
+  }
 });
